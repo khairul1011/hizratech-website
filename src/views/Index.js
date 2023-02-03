3b3469eb6600930bfffdecb0a9546b1502740463
khairul1011 <erul0575@gmail.com>
2023-02-03 16:08:33 +0700

    Main delete github link

--- src/views/Index.js
+++ src/views/Index.js
@@ -35,7 +35,7 @@ export default function Index() {
                   target="_blank"
                   className="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
                 >
-                  Get started
+                  Mulai
                 </a>
 
               </div>
